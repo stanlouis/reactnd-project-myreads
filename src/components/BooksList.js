81398b25578f7c32028ca56a88f9d5178ce520f0
Stanley Louis <stanlouis@msn.com>
2018-09-04 22:18:43 -0400

    Refactor BooksList per reviewer suggestion.

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -6,43 +6,26 @@ import BookShelf from './BookShelf';
 export default class BooksList extends Component {
   render() {
 
+    const shelves = {
+      currentlyReading: ['Currently Reading', 'currentlyReading'],
+      wantToRead: ['Want to Read', 'wantToRead'],
+      read: ['Read', 'read']
+    }
+
     return (
       <div className="list-books">
         <AppTitle title="MyReads" />
 
         <div className="list-books-content">
-          <div>
-            <div className="bookshelf">
-              <h2 className="bookshelf-title">Currently Reading</h2>
-              {/* bookshelf */}
+          { Object.keys(shelves).map((shelf) =>
+            <div className="bookshelf" key={shelf}>
+              <h2 className="bookshelf-title">{shelves[shelf][0]}</h2>
               <BookShelf
-                selectBooks={this.props.books.filter(
-                  book => book.shelf === 'currentlyReading'
-                )}
+                selectBooks={this.props.books.filter(book => book.shelf === shelves[shelf][1])}
                 onShelfChange={this.props.onShelfChange}
               />
             </div>
-            <div className="bookshelf">
-              <h2 className="bookshelf-title">Want to Read</h2>
-              {/* bookshelf */}
-              <BookShelf
-                selectBooks={this.props.books.filter(
-                  book => book.shelf === 'wantToRead'
-                )}
-                onShelfChange={this.props.onShelfChange}
-              />
-            </div>
-            <div className="bookshelf">
-              <h2 className="bookshelf-title">Read</h2>
-              {/* bookshelf */}
-              <BookShelf
-                selectBooks={this.props.books.filter(
-                  book => book.shelf === 'read'
-                )}
-                onShelfChange={this.props.onShelfChange}
-              />
-            </div>
-          </div>
+          )}
         </div>
         <div className="open-search">
           <Link to="/search" className="add-contact">
